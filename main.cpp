63c2afb8e2cb0e35aed6f630f555de4b920b4ea9
Rhubarb06150 <matheogass@outlook.com>
2024-06-04 23:10:14 +0200

    working on computer functions

--- main.cpp
+++ main.cpp
@@ -137,7 +137,7 @@ class Game{
         moving_timer=0;
         function_done=0;
         username="Player";
-        player_type="b";
+        player_type="g";
         player_pos={500,500};
         can_move=true;
         debug=false;
