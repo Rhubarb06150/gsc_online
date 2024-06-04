c816c41d17fb17f88d81d23ef1192547a9bd0533
Rhubarb06150 <matheogass@outlook.com>
2024-06-04 22:18:19 +0200

    working on computer functions

--- functions/terrain.cpp
+++ functions/terrain.cpp
@@ -50,6 +50,7 @@ class Terrain{
         while (getline(inputFile, line)){
             terrain_vector.push_back({});
             for (int i = 0; i< line.length()/4; i++){
+                std::cout << "adding " << line.substr(i*4, 4) << std::endl;    
                 terrain_vector[y].push_back(line.substr(i*4, 4));
             };
             y++;
