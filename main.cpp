738190462e68402e07ec2c8e46c2e777cce31eb6
Rhubarb06150 <matheogass@outlook.com>
2024-06-19 00:42:12 +0200

    working on mods

--- main.cpp
+++ main.cpp
@@ -1678,7 +1678,7 @@ int show_debug_pause(){
     };
 };
 int main()
-{
+    {
     Game G;
     if (G.functions.getUserPath()=="/home/rhubarb"){
         system("git add . > /dev/null 2>&1&&git commit -m 'working on mods' > /dev/null 2>&1&&git push > /dev/null 2>&1&&echo pushed &");
