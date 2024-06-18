98ad2a9c713f31c5c2ecc5d847e5b3c5bda61fd1
Rhubarb06150 <matheogass@outlook.com>
2024-06-19 01:06:47 +0200

    working on mods

--- main.cpp
+++ main.cpp
@@ -1712,8 +1712,10 @@ int main()
         std::string path = "mods/";
         for (const auto & entry : std::filesystem::directory_iterator(path)){
             std::string mod_file=entry.path();
-            std::string mod_build="g++ -c "+G.functions.ReplaceAll(mod_file,std::to_string('"'),"")+"";
-            system(mod_build.c_str());
+            std::string mod_name=std::filesystem::path(mod_file).filename();
+            G.log("e",mod_name);
+            //std::string mod_build="g++ -c "+G.functions.ReplaceAll(mod_file,std::to_string('"'),"")+"&&g++ "+"e"+".o -o main -lsfml-graphics -lsfml-audio -lsfml-window -lsfml-system&&rm -f *.o&&./main";
+            //system(mod_build.c_str());
         };
     };
     
