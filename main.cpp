68a00236601e5c96de61779459c094a0e4c1b1ec
Rhubarb06150 <matheogass@outlook.com>
2024-06-18 23:51:16 +0200

    working on animated tiles

--- main.cpp
+++ main.cpp
@@ -262,8 +262,8 @@ int crash(std::string message){
             };
         };
         window.clear();
-        HUDdisplay.showTextWOB("Game crashed",{32,32},window);
-        HUDdisplay.showTextDEBUG(message,{0,64},window);
+        HUDdisplay.showTextWOB("Game crashed",{0,0},window);
+        HUDdisplay.showTextDEBUG(message,{0,16},window);
         HUDdisplay.showTextDEBUG("Press any key to quit the game",{0,560},window);
         window.display();
     }while(true);
