dc6b09010105e5e728f9c5ff378f1d06a12d8144
Rhubarb06150 <matheogass@outlook.com>
2024-06-19 15:46:17 +0200

    working on mods

--- main.cpp
+++ main.cpp
@@ -1750,8 +1750,7 @@ int main()
         //G.crash("g++ is not installed on this system.\nIf you want to use mods,\nyou need to have g++ intalled.");
     };
     #endif
-    //G.loadMods();
-
+    G.loadMods();
     if (!std::filesystem::is_directory("/tmp/.gsc_o/")){
         std::filesystem::create_directory("/tmp/.gsc_o/");
         std::filesystem::create_directory("/tmp/.gsc_o/mods/");
@@ -1759,7 +1758,7 @@ int main()
     }else if (!std::filesystem::is_directory("/tmp/.gsc_o/mods/")){
         std::filesystem::create_directory("/tmp/.gsc_o/mods/");
     };
-    
+
     if (!std::filesystem::is_directory(G.functions.getUserPath()+"/.gsc_o/")){
         std::filesystem::create_directory(G.functions.getUserPath()+"/.gsc_o");
         std::filesystem::create_directory(G.functions.getUserPath()+"/.gsc_o/screenshots/");
