39ec7048593b259080c2695500963532b5c7d951
Rhubarb06150 <matheogass@outlook.com>
2024-06-20 12:17:03 +0200

    working on mods

--- modBuilder.cpp
+++ modBuilder.cpp
@@ -53,7 +53,7 @@ class Main{
         };
         
         F.createMissingDir("/tmp/.gsc_o/");
-        system("rm -r /tmp/.gsc_o/source -f  > /dev/null 2>&1");
+        system("rm -r /tmp/.gsc_o/source -f > /dev/null 2>&1");
         F.log("CONFIG","Setup of the temp source folder");
         F.createMissingDir("/tmp/.gsc_o/source/");
         F.log("INFO","Getting your version source code from github...");
@@ -165,10 +165,10 @@ class Main{
         build_str="g++ -c /tmp/.gsc_o/source/main.cpp&&g++ main.o -o GSC_Online_Modded -lsfml-graphics -lsfml-audio -lsfml-window -lsfml-system&&rm -f main.o";
         if (system(build_str.c_str())==0){
             F.log("INFO","Game built with success!");
-            system("rm -r /tmp/.gsc_o/* > /dev/null 2>&1");
+            system("rm -r /tmp/.gsc_o/* -f > /dev/null 2>&1");
         }else{
             F.log("ERROR","Failed to build the game, verify if mods doesn't contain errors.");
-            system("rm -r /tmp/.gsc_o/* > /dev/null 2>&1");
+            system("rm -r /tmp/.gsc_o/* -f > /dev/null 2>&1");
             exit(5);
         };
         return 0;
