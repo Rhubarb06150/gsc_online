43c5fe99a10c01eda8b65d5eb820fee3287c90f2
Rhubarb06150 <matheogass@outlook.com>
2024-06-20 11:20:37 +0200

    working on mods

--- modBuilder.cpp
+++ modBuilder.cpp
@@ -88,6 +88,7 @@ class Main{
                             std::string inst;
                             F.log("INFO","Copying "+mod_paths[i]+" in build folder...");
                             inst="cp '"+mod_paths[i]+"' /tmp/.gsc_o/source/mods";
+                            system(inst.c_str());
                             if (system(inst.c_str())==0){
                                 F.log("ERROR","failed to copy "+mod_paths[i]+" in build folder");
                             };
