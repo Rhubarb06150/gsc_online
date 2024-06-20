b96c8edce8d9a73d68a0e77284e055e1081e60a3
Rhubarb06150 <matheogass@outlook.com>
2024-06-20 14:53:47 +0200

    working on mods

--- main.cpp
+++ main.cpp
@@ -1448,11 +1448,13 @@ int modManager(){
             };
         };
         for (int i = 0; i<mods_list.size();i++){
-        if (i>5)break;
             if (mods_states[i]==true){
             //DISPLAY ACTIVE MOD
             }else{
             //DISPLAY UnACTIVE MOD
+            };
+            if (i>=6){
+                break;
             };
         };
         if (choice>=6){
