12915bcbfacd7e9414bbdf46a9f6b6087f8d72b1
Rhubarb06150 <matheogass@outlook.com>
2024-06-20 12:37:57 +0200

    working on mods

--- main.cpp
+++ main.cpp
@@ -1419,6 +1419,7 @@ int levelEditorLoop(sf::RenderWindow&window,sf::Event event){
 int main_menu(){
     int choice = 0;
     bool choosen=false;
+    int max_choice=3+(mods_list.size())>0;
     functions.log("INFO","Opening main menu");
     do{
         while (window.pollEvent(event))
@@ -1432,7 +1433,7 @@ int main_menu(){
                         break;
                 };
                 if (event.key.code==sf::Keyboard::Down){
-                    if (choice<4){
+                    if (choice<choosen){
                         choice++;
                     };
                     break;
