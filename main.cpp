4c13c40062e1c2d9134681950e7128994af9caa4
Rhubarb06150 <matheogass@outlook.com>
2024-06-20 14:42:25 +0200

    working on mods

--- main.cpp
+++ main.cpp
@@ -1441,7 +1441,7 @@ int modManager(){
                     };
                 };
                 if (event.key.code==sf::Keyboard::Down){
-                    if (choice<mods_list.size()-1){
+                    if (choice<mods_list.size()-6){
                         choice++;
                     };
                 };
