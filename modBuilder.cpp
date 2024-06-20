9ac5e602fc522d3028875a146ad96cb62b99b71a
Rhubarb06150 <matheogass@outlook.com>
2024-06-20 13:19:46 +0200

    working on mods

--- modBuilder.cpp
+++ modBuilder.cpp
@@ -180,7 +180,7 @@ class Main{
                 line=F.ReplaceAll(line,"//MOD INIT",inits);
                 line=F.ReplaceAll(line,"//MOD ACT",acts);
                 line=F.ReplaceAll(line,"std::vector<std::string> mods_list;",types);
-                line=F.ReplaceAll(line,"//DISPLAY MOD NAME","HUDdisplay.showTextBOW(mods_list[i].name);");
+                line=F.ReplaceAll(line,"//DISPLAY MOD NAME","HUDdisplay.showTextBOW(mods_names_list[i]);");
                 line=F.ReplaceAll(line,"mods_list={};",mod_list);
                 line=F.ReplaceAll(line,"mods_names_list={};",mod_list);
                 main_output<<line<<"\n";
