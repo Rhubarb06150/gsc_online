e3a80807da0df7641e677f1f7fb0ef07d4ce8ba7
Rhubarb06150 <matheogass@outlook.com>
2024-06-21 09:40:32 +0200

    working on mods

--- mods/miniMap.cpp
+++ mods/miniMap.cpp
@@ -88,7 +88,7 @@ class MOD_MiniMAP{
                 if (((width*8)-(cur_player_pos[0]/8)+64)>size-24){
                     break;
                 }
-                if (cols>(size/8)+1){
+                if ((cols>(size/8)+1)*(zoom*2)){
                     break;
                 };
         };
@@ -96,7 +96,7 @@ class MOD_MiniMAP{
         if ((height*8-(cur_player_pos[1]/8)+64)>size-24){
             break;
         }
-        if (rows>(size/8)+2){
+        if ((rows>(size/8)+2)*(zoom*2)){
             break;
         }
         };
