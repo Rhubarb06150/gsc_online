233927e4abade19cabe40b208f215524987c72a7
Rhubarb06150 <matheogass@outlook.com>
2024-06-21 10:21:09 +0200

    working on mods

--- mods/miniMap.cpp
+++ mods/miniMap.cpp
@@ -58,7 +58,7 @@ class MOD_MiniMAP{
     int act(sf::Event event){
         return 0;
     };
-    int show(sf::RenderWindow& window, std::string time_otd_str, bool debug){
+    int show(sf::RenderWindow& window, char time_otd, bool debug){
         if (show_minimap){
             if (!debug){
             sf::RectangleShape rectangle(sf::Vector2f(size,size));
@@ -79,9 +79,9 @@ class MOD_MiniMAP{
 
             int time_index=0;
 
-            if (time_otd_str=="m"){time_index=0;}
-            else if (time_otd_str=="d"){time_index=1;}
-            else if (time_otd_str=="n"){time_index=2;}
+            if (time_otd=='m'){time_index=0;}
+            else if (time_otd=='d'){time_index=1;}
+            else if (time_otd=='n'){time_index=2;}
 
             int rows=0;
             int shown_sprites=0;
