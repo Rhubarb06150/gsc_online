68b67fad0923688a3ad56e8eb559687ae68eb152
Rhubarb06150 <matheogass@outlook.com>
2024-06-25 16:24:37 +0200

    working on level editor

--- functions/hud.cpp
+++ functions/hud.cpp
@@ -218,7 +218,8 @@ class HUD{
         for(int i=0;i<(size[0]/32)-64;i++){
             sf::Sprite bar;
             bar.setTexture(border_textures[4]);
-            float bar_pos=(i*32)+pos[0];
+            float bar_pos;
+            bar_pos=(i*32)+pos[0];
             bar.setPosition({bar_pos,pos[1]});
             window.draw(bar);
         };
