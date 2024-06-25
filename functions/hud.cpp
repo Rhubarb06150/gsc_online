1597b27811cc6fb61479263015f6d872ab147659
Rhubarb06150 <matheogass@outlook.com>
2024-06-26 00:06:10 +0200

    working on level editor

--- functions/hud.cpp
+++ functions/hud.cpp
@@ -342,7 +342,7 @@ class HUD{
     int showLogo(std::vector<int> text_pos, sf::RenderWindow& window){
         sf::Sprite sprite;
         sprite.setPosition(text_pos[0],text_pos[1]);
-        sprite.setTexture(menu_textures[2]);
+        sprite.setTexture(menu_textures[1]);
         sprite.scale(4.f,4.f);
         window.draw(sprite);
         return 0;  
