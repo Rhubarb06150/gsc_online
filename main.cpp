38c9e8b5d6ff1a70d34ea44769186e41f95a607b
Rhubarb06150 <matheogass@outlook.com>
2024-06-26 00:00:34 +0200

    working on level editor

--- main.cpp
+++ main.cpp
@@ -1952,7 +1952,7 @@ int main(int argc, char** argv)
     G.functions.createMissingDir(G.functions.getUserPath()+"/.gsc_o/screenshots/");
     G.functions.createMissingDir(G.functions.getUserPath()+"/.gsc_o/maps/");
     G.functions.createMissingDir(G.functions.getUserPath()+"/.gsc_o/renders/");
-    G.functions.log("INFO","an game folder has been created at "+G.functions.getUserPath()+"/.gsc_o, it will be used to store your saved maps and your screenshots");
+    //G.functions.log("INFO","an game folder has been created at "+G.functions.getUserPath()+"/.gsc_o, it will be used to store your saved maps and your screenshots");
 
     G.main_menu();
     while (G.window.isOpen()){
