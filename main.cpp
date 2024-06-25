dd09fd539dd7c48db20f4d3551bfdadfa196bc8f
Rhubarb06150 <matheogass@outlook.com>
2024-06-25 13:10:40 +0200

    working on mods

--- main.cpp
+++ main.cpp
@@ -1352,6 +1352,7 @@ int levelEditorLoop(sf::RenderWindow&window,sf::Event event){
                                         for (int i=0;i<terrain_vector[0].size();i++){
                                             terrain_line.push_back(Tiles.tiles[default_tile_index][2]);
                                         };
+                                        terrain_vector.push_back(terrain_line);
                                     };
                                 }else if (map_height>height_convert){
                                     while(map_height!=height_convert){//REMOVING HEIGHT
