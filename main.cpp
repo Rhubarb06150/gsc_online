b46ed506fae017fbcf9e24b983517949babdc9b8
Rhubarb06150 <matheogass@outlook.com>
2024-06-25 12:13:18 +0200

    working on mods

--- main.cpp
+++ main.cpp
@@ -1242,13 +1242,13 @@ int levelEditorLoop(sf::RenderWindow&window,sf::Event event){
                             };
                             terrain_vector.push_back(terrain_line);
                         }else if(choice==3){
-                            if (default_tile_index>Tiles.tiles.size()-1){
+                            if (default_tile_index>Tiles.tiles.size()){
                                 default_tile_index++;
                             }else{
                                 default_tile_index=0;
                             };
                         }else if(choice==4){
-                            if (selected_tile_index>Tiles.tiles.size()-1){
+                            if (selected_tile_index>Tiles.tiles.size()){
                                 selected_tile_index++;
                             }else{
                                 selected_tile_index=0;
