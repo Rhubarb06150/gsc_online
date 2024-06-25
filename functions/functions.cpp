16620f48531eb90da8a007f5b4e92d87ee18b15d
Rhubarb06150 <matheogass@outlook.com>
2024-06-25 11:33:38 +0200

    working on mods

--- functions/functions.cpp
+++ functions/functions.cpp
@@ -144,7 +144,7 @@ class GSC_Functions{
     };
 
     int saveMap(std::vector<std::vector<std::string>> terrain_tiles, std::string map_path){
-        std::ofstream map_file(getUserPath()+".gsc_o/maps/"+map_path+".lv");
+        std::ofstream map_file(map_path);
         for (int i = 0; i < terrain_tiles.size() ; i++){
             for (int j = 0; j < terrain_tiles[i].size();j++){
                 map_file << terrain_tiles[i][j];
