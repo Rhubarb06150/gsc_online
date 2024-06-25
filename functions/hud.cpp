d3a597a7f06224970b6664b89017f83418b7e420
Rhubarb06150 <matheogass@outlook.com>
2024-06-25 16:18:05 +0200

    working on level editor

--- functions/hud.cpp
+++ functions/hud.cpp
@@ -202,7 +202,7 @@ class HUD{
         float pos0tl=pos[0];
         float pos1tl=pos[1];
         float pos0tr=pos[0]+size[0]-64;
-        float pos1tr=pos[1]-64;
+        float pos1tr=pos[1];
         float pos0bl=pos[0];
         float pos1bl=pos[1]+size[1];
         float pos0br=pos[0]+size[0]-64;
