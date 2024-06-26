f550333627b71984b710d6e5a32abb9d8a524f26
Rhubarb06150 <matheogass@outlook.com>
2024-06-26 10:17:45 +0200

    working on level editor

--- functions/hud.cpp
+++ functions/hud.cpp
@@ -129,7 +129,7 @@ class HUD{
         letters_index.push_back({"?","de"});
         letters_index.push_back({"!","ee"});
         letters_index.push_back({"\xee","cf"});
-        letters_index.push_back({"\x01","df"});
+        letters_index.push_back({"\xef","df"});
         letters_index.push_back({"","de"});
 
         // MENU TEXTURES
