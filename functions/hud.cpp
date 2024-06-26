44c86d80e795a6b9de201a8f397b46079baf308d
Rhubarb06150 <matheogass@outlook.com>
2024-06-26 11:30:16 +0200

    working on level editor

--- functions/hud.cpp
+++ functions/hud.cpp
@@ -178,9 +178,9 @@ class HUD{
             border_textures.push_back(texture);
             texture.loadFromFile("assets/hud/border.png",sf::IntRect(16,0*k,8,8));//TOP RIGHT CORNER
             border_textures.push_back(texture);
-            texture.loadFromFile("assets/hud/border.png",sf::IntRect(16,8*k,8,8));//BOT LEFT CORNER
+            texture.loadFromFile("assets/hud/border.png",sf::IntRect(8,8*k,8,8));//BOT LEFT CORNER
             border_textures.push_back(texture);
-            texture.loadFromFile("assets/hud/border.png",sf::IntRect(8,8*k,8,8));//BOT RIGHT CORNER
+            texture.loadFromFile("assets/hud/border.png",sf::IntRect(16,8*k,8,8));//BOT RIGHT CORNER
             border_textures.push_back(texture);
             texture.loadFromFile("assets/hud/border.png",sf::IntRect(8,0*k,8,8));//ROW CORNER
             border_textures.push_back(texture);
