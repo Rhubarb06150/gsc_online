714c50d5dc128853c8d48fb523a902961a4937a9
Rhubarb06150 <matheogass@outlook.com>
2024-06-26 11:46:36 +0200

    working on user interface

--- main.cpp
+++ main.cpp
@@ -201,7 +201,7 @@ class Game{
         mods_list={};
         mods_states={};
         mods_descriptions={};
-        border_style=5;
+        border_style=1;
         //MOD DESC
         srand(time(NULL));//reset the random values
 
