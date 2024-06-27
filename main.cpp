4581a42b9321516334992fae8ddee29502a4e833
Rhubarb06150 <matheogass@outlook.com>
2024-06-27 11:36:44 +0200

    working on user interface

--- main.cpp
+++ main.cpp
@@ -2014,7 +2014,7 @@ int packMenu(){
         HUDdisplay.drawSquare(window,{0,384},{640,192},border_style);
         for(int i=0;i<pocket_content.size();i++){
             HUDdisplay.showTextBOW(pocket_content[i][0],{256,96+(i*64)},window);//SHOWING ITEM NAME
-            HUDdisplay.showTextBOW(ItemsIndex.items[ItemsIndex.getItemIntIndexByName(pocket_content[i][0])][2],{32,448},window);//SHOWING ITEM DESCRIPTION
+            HUDdisplay.showTextBOW(ItemsIndex.items[ItemsIndex.getItemIntIndexByName(pocket_content[i][0])][3],{32,448},window);//SHOWING ITEM DESCRIPTION
         };
         window.display();
     }while(true);
