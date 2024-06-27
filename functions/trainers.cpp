8c5a6aa4bf316c4b2bba0533a29ed6fc58b1453a
Rhubarb06150 <matheogass@outlook.com>
2024-06-27 14:37:42 +0200

    working on user interface

---
+++ functions/trainers.cpp
@@ -0,0 +1,43 @@
+#pragma once
+
+#include <SFML/Graphics.hpp>
+#include <vector>
+#include <string>
+#include <iostream>
+
+class Trainers{
+    public:
+    std::vector<std::vector<std::string>> trainers_index;
+    std::vector<sf::Texture> trainers_textures;
+    Trainers(){
+        int trainers_nb=0;
+        for(int i=0;i<8;i++){
+            for(int j=0;j<9;j++){
+                trainers_index.push_back({"trainer"+trainers_nb,std::to_string(i)+std::to_string(j)});
+                trainers_nb++;
+            };
+        };
+        int x;
+        int y;
+        for(int i=0;i<trainers_index.size();i++){
+            sf::Texture texture;
+            std::string fish = trainers_index[i][1];
+            x=std::stoi(""+fish[0]);
+            y=std::stoi(""+fish[1]);
+            texture.loadFromFile("assets/hud/menus.png",sf::IntRect(x*56,y*56,56,56));
+            trainers_textures.push_back(texture);
+        };
+    };
+    int showTrainer(sf::RenderWindow& window,std::vector<int> pos, int id){
+        for(int i=0;i<trainers_index.size();i++){
+            if (trainers_index[i][0]=="trainer"+std::to_string(id)){
+                sf::Sprite trainer;
+                trainer.setTexture(trainers_textures[i]);
+                trainer.setPosition(pos[0],pos[1]);
+                window.draw(trainer);
+                return 0;
+            };
+        };
+        return 0;
+    };
+};
