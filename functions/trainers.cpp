9f96782119984bf694fc3d7617782eee7015b4d4
Rhubarb06150 <matheogass@outlook.com>
2024-06-27 15:15:53 +0200

    working on user interface

--- functions/trainers.cpp
+++ functions/trainers.cpp
@@ -28,6 +28,7 @@ class Trainers{
             ystr=fish[1];
             x=std::stoi(xstr);
             y=std::stoi(ystr);
+            std::cout<<"X:"<<x<<"Y:"<<y<<std::endl;
             texture.loadFromFile("assets/hud/menus.png",sf::IntRect(x*56,y*56,56,56));
             trainers_textures.push_back(texture);
         };
