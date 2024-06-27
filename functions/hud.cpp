b92f729512eec74188af1f784672115c9ce13e21
Rhubarb06150 <matheogass@outlook.com>
2024-06-27 11:44:23 +0200

    working on user interface

--- functions/hud.cpp
+++ functions/hud.cpp
@@ -445,7 +445,7 @@ class HUD{
     int showRedCursor(std::vector<int> text_pos, sf::RenderWindow& window){
         sf::Sprite sprite;
         sprite.setPosition(text_pos[0],text_pos[1]);
-        sprite.setTexture(menu_tiles[32]);
+        sprite.setTexture(menu_tiles[31]);
         sprite.scale(4.f,4.f);
         window.draw(sprite);
         return 0;  
