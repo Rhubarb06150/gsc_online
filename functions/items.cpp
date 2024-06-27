8e89138aa94cf7905e9070bd316a415cb33e6e1c
Rhubarb06150 <matheogass@outlook.com>
2024-06-27 11:37:04 +0200

    working on user interface

--- functions/items.cpp
+++ functions/items.cpp
@@ -5,7 +5,7 @@ class Items{
     public:
     std::vector<std::vector<std::string>> items;
     Items(){
-        items.push_back({"ITEMS","\x00","NONE","Item not exist."});
+        items.push_back({"ITEMS","\x00","???","?????"});
         items.push_back({"BALLS","\x01","MASTER BALL","The best Ball.\n\nIt never misses."});
         items.push_back({"BALLS","\x02","ULTRA BALL","A Ball with a high\n\nrate of success."});
         items.push_back({"ITEMS","\x03","BRIGHTPOWDER","Lowers the foe's\n\naccuracy. (HOLD)"});
