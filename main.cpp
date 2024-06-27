2ef016e576fbcc8b4a23fc463dfe10da39f9ba57
Rhubarb06150 <matheogass@outlook.com>
2024-06-27 16:34:04 +0200

    working on user interface

--- main.cpp
+++ main.cpp
@@ -226,8 +226,7 @@ void screenshotThread(){
     while(true){
         if (event.type==sf::Event::KeyPressed){
             if (event.key.code==sf::Keyboard::F1){
-                while(full_loaded==0){
-                usleep(500);
+                while(full_loaded!=1){
                 };
                 functions.takeScreenshot(window,index_frame);
                 SoundManager.soundEffect("PRESS");
