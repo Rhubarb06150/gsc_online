f5ef0807814fbeb6bddd62ba2dd00559dd2d056b
Rhubarb06150 <matheogass@outlook.com>
2024-06-27 11:52:00 +0200

    working on user interface

--- main.cpp
+++ main.cpp
@@ -1980,7 +1980,17 @@ int packMenu(){
     int choice=0;
     int offset=0;
     std::vector<std::vector<std::string>> pocket_content;
-    pocket_content={{ItemsIndex.items[ItemsIndex.items.size()-1][2],"1"},{ItemsIndex.items[ItemsIndex.items.size()-2][2],"2"}};
+    pocket_content={
+        {ItemsIndex.items[ItemsIndex.items.size()-1][2],"1"},
+        {ItemsIndex.items[ItemsIndex.items.size()-2][2],"2"},
+        {ItemsIndex.items[ItemsIndex.items.size()-2][2],"2"},
+        {ItemsIndex.items[ItemsIndex.items.size()-2][2],"2"},
+        {ItemsIndex.items[ItemsIndex.items.size()-2][2],"2"},
+        {ItemsIndex.items[ItemsIndex.items.size()-2][2],"2"},
+        {ItemsIndex.items[ItemsIndex.items.size()-2][2],"2"},
+        {ItemsIndex.items[ItemsIndex.items.size()-2][2],"2"},
+        {ItemsIndex.items[ItemsIndex.items.size()-2][2],"2"},
+        {ItemsIndex.items[ItemsIndex.items.size()-2][2],"2"}};
     do{
         while (window.pollEvent(event)){
             if(event.type==sf::Event::KeyPressed){
