4070f5c228604f2743d617d707bcf14906a1bc61
Rhubarb06150 <matheogass@outlook.com>
2024-06-28 10:39:53 +0200

    working on user interface

--- functions/functions.cpp
+++ functions/functions.cpp
@@ -219,6 +219,9 @@ class GSC_Functions{
                 image=cur_texture.copyToImage();
                 for(int ytex=0;ytex<cur_texture.getSize().y;ytex++){
                     for(int xtex=0;xtex<cur_texture.getSize().x;xtex++){
+                        if(terrain_vector[i][j]=="\xff"){
+                            std::cout << image.getPixel(xtex,ytex).r<< image.getPixel(xtex,ytex).g<< image.getPixel(xtex,ytex).b<< image.getPixel(xtex,ytex).a << std::endl;
+                        };
                         render_image.setPixel((j*16)+ytex,(i*16)+xtex,image.getPixel(xtex,ytex));
                     };
                 };
