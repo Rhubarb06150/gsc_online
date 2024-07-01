910235c3c2284fa84791213bb228c8649b930ef9
Rhubarb06150 <matheogass@outlook.com>
2024-07-01 14:27:15 +0200

    working on user interface

--- main.cpp
+++ main.cpp
@@ -236,7 +236,7 @@ int showMessage(std::string message){
         terrain.showTerrain(terrain_vector,player_pos,time_otd,window,player_offset,false,index_frame);
         player.showPlayer(player_type,player_state,window);
         HUDdisplay.drawSquare(window,{0,352},{640,224},border_style);
-        HUDdisplay.showTextBOW(message_cur,{32,384},window);
+        HUDdisplay.showTextBOW(message_cur,{32,416},window);
         window.display();
         letters_shown++;
     }while(true);
