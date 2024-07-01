598bfcc68d848399dde0128605223c78e6482b09
Rhubarb06150 <matheogass@outlook.com>
2024-07-01 15:23:30 +0200

    working on user interface

--- main.cpp
+++ main.cpp
@@ -288,7 +288,7 @@ int showMessage(std::string message){
                 message_cur.erase(0,19);
             };
             if(transition_frame==8){
-                offset-=4;
+                offset-=2;
                 letters_shown-=19;
                 transition_frame=0;
                 transition=false;
