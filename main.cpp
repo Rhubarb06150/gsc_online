daa5360b3da54f69290e4167880872955987a2fe
Rhubarb06150 <matheogass@outlook.com>
2024-07-02 11:43:33 +0200

    working on user interface

--- main.cpp
+++ main.cpp
@@ -238,7 +238,7 @@ int showMessage(std::string message,bool yes_no){
     bool transition=false;
     int transition_frame=0;
     if(letters_nb<39&&letters_nb>18){
-        message.insert(17,"\n\n");
+        message.insert(18,"\n\n");
     }
     for(int i=2;i<message.length();i++){
         if(i!=0 && i%20==0 && i!=letters_nb){
