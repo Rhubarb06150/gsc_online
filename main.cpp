0a8eee5a76f3b6ddf9e5a43380f853e75c574dca
Rhubarb06150 <matheogass@outlook.com>
2024-07-02 10:37:15 +0200

    working on user interface

--- main.cpp
+++ main.cpp
@@ -238,12 +238,14 @@ int showMessage(std::string message){
     for(int i=2;i<message.length();i++){
         std::cout<<i<<std::endl;
          if(i!=0 && i%(20-space)==0 && i!=letters_nb){
-            space=0;
-            std::cout<<"espace"<<std::endl;
-            //std::cout<<"taille: "<<message.length()<<std::endl;
-            message.insert(i,"\n\n");
-            letters_nb+=2;
-            //std::cout<<"nouvelle taille: "<<message.length()<<std::endl;
+            if(i!=18){
+                space=0;
+                std::cout<<"espace"<<std::endl;
+                //std::cout<<"taille: "<<message.length()<<std::endl;
+                message.insert(i,"\n\n");
+                letters_nb+=2;
+                //std::cout<<"nouvelle taille: "<<message.length()<<std::endl;
+            };
         };
     };
     std::cout<<"Message:"<<std::endl;
