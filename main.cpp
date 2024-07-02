5004a900f0166ce0861afccc2be7f46b75f82ebc
Rhubarb06150 <matheogass@outlook.com>
2024-07-02 10:32:23 +0200

    working on user interface

--- main.cpp
+++ main.cpp
@@ -238,6 +238,7 @@ int showMessage(std::string message){
     for(int i=0;i<message.length();i++){
         std::cout<<i<<std::endl;
          if(i!=0&&
+            i!=18&&
             i%(20-space)==0&&
             i!=letters_nb){
             space=0;
