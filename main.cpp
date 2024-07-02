83a1d5d9c6adb79d08f3107e6d77a199a0caa71f
Rhubarb06150 <matheogass@outlook.com>
2024-07-02 11:49:26 +0200

    working on user interface

--- main.cpp
+++ main.cpp
@@ -2389,7 +2389,7 @@ int main(int argc, char** argv)
     G.loadSettings();
     //G.functions.log("INFO","an game folder has been created at "+G.functions.getUserPath()+"/.gsc_o, it will be used to store your saved maps and your screenshots");
     G.main_menu();
-    G.showMessage("Do you want a gift???",true);
+G.showMessage("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAB",true);
     while (G.window.isOpen()){
         G.mainLoop();
     };
