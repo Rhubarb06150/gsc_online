546607b62c80fd4945c468ed4e8e6738a9668d43
Rhubarb06150 <matheogass@outlook.com>
2024-07-02 10:27:06 +0200

    working on user interface

--- main.cpp
+++ main.cpp
@@ -234,10 +234,11 @@ int showMessage(std::string message){
     bool finished=false;
     bool transition=false;
     int transition_frame=0;
-    int space=0;
+    int space=2;
     for(int i=2;i<message.length();i++){
         std::cout<<"space: "<<space<<std::endl;
-        if(i!=0&&i%(20)==0&&i!=letters_nb){
+        if(i!=0&&i%(20-space)==0&&i!=letters_nb){
+            space=0;
             std::cout<<"espace"<<std::endl;
             //std::cout<<"taille: "<<message.length()<<std::endl;
             message.insert(i,"\n\n");
