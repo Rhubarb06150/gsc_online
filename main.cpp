0e2b12c998896ad1656c3d3dd60d1276a6acfdc6
Rhubarb06150 <matheogass@outlook.com>
2024-07-02 11:19:44 +0200

    working on user interface

--- main.cpp
+++ main.cpp
@@ -2378,7 +2378,7 @@ int main(int argc, char** argv)
     G.loadSettings();
     //G.functions.log("INFO","an game folder has been created at "+G.functions.getUserPath()+"/.gsc_o, it will be used to store your saved maps and your screenshots");
     G.main_menu();
-    G.showMessage("What are you doing here?");
+    G.showMessage("Pokémon");
     while (G.window.isOpen()){
         G.mainLoop();
     };
